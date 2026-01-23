2a49210678e6097d2d87388b5236a602bbf4d814
Mark Drew <mark.drew@gmail.com>
2026-01-23 17:02:54 +0000

    Initial commit of our latest version

---
+++ Formula/lucli.rb
@@ -0,0 +1,37 @@
+class Lucli < Formula
+  desc "CLI for Lucee CFML (LuCLI)"
+  homepage "https://github.com/cybersonic/LuCLI"
+  version "0.1.266"
+
+  on_macos do
+    url "https://github.com/cybersonic/LuCLI/releases/download/v0.1.266/lucli-0.1.266-macos"
+    sha256 "3c1b41555cfe59ccd8fbd419d01aa7d6b0a96f7707fa421795ac231855048686"
+  end
+
+  on_linux do
+    url "https://github.com/cybersonic/LuCLI/releases/download/v0.1.266/lucli-0.1.266-linux"
+    sha256 "d132f5d5dbd7603e675f373a8c491bf758d65eec4e3f3193cad6a0350ac2f30f"
+  end
+
+  # If these binaries require Java on PATH, you can add:
+  # depends_on "openjdk"
+
+  def install
+    # Determine the downloaded filename based on OS
+    downloaded = if OS.mac?
+      "lucli-#{version}-macos"
+    else
+      "lucli-#{version}-linux"
+    end
+
+    # Install as "lucli" in libexec
+    libexec.install downloaded => "lucli"
+
+    # Create wrapper in bin so Homebrew puts it on PATH
+    bin.write_exec_script libexec/"lucli"
+  end
+
+  test do
+    system "#{bin}/lucli", "--version"
+  end
+end
