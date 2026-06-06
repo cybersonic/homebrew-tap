07d7d0979572ce804d9a7d3e9b985b0cd4a21559
Mark Drew <mark.drew@gmail.com>
2026-06-06 23:31:18 +0100

    fix(markspresso): add openjdk runtime env wrapper

--- Formula/markspresso.rb
+++ Formula/markspresso.rb
@@ -4,11 +4,12 @@ class Markspresso < Formula
   version "1.0.3"
   url "https://github.com/cybersonic/markspresso/releases/download/v1.0.3/markspresso"
   sha256 "af7099ca77b3e1c5ecbeaaa2c6d76eb3107bd6d336817e40332b1131336d15cb"
+  depends_on "openjdk"
 
   def install
     libexec.install "markspresso"
     chmod 0755, libexec/"markspresso"
-    bin.write_exec_script libexec/"markspresso"
+    bin.write_env_script libexec/"markspresso", JAVA_HOME: Formula["openjdk"].opt_prefix
   end
 
   test do
